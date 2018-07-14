45e6373b5eb5e4daf1af693b78aef7e090f654b5
Harrison Mendonça <harrison@grama.cc>
2018-07-13 22:40:00 -0300

    Food (#2)
    * add food
    
    * add avocado poison
    
    * eat food
    
    * remove pipe
    
    * generate random food by id
    
    * use weight
    
    * updade mate data
    
    * use equal values
    
    * resize birds
    
    * improve boundries
    
    * add var differentTypesOfFood
    
    * balance

--- game.js
+++ game.js
@@ -26,6 +26,7 @@ var Neuvol;
 var game;
 var FPS = 60;
 var maxScore=0;
+var differentTypesOfFood = 10;
 
 var images = {};
 
@@ -60,6 +61,7 @@ var Bird = function(json){
 	this.gravity = 0;
 	this.velocity = 0.3;
 	this.jump = -6;
+	this.weight = 75;
 
 	this.init(json);
 }
@@ -70,67 +72,58 @@ Bird.prototype.init = function(json){
 	}
 }
 
-Bird.prototype.flap = function(){
-	this.gravity = this.jump;
+Bird.prototype.checkFood = function(food, fed){
+	if (fed) {
+		this.weight += food.isPoison ? -20 : 5;
+	}
+	this.fedLastFood = fed;
 }
 
 Bird.prototype.update = function(){
-	this.gravity += this.velocity;
-	this.y += this.gravity;
+	this.weight -= 0.02;
 }
 
-Bird.prototype.isDead = function(height, pipes){
-	if(this.y >= height || this.y + this.height <= 0){
-		return true;
-	}
-	for(var i in pipes){
-		if(!(
-			this.x > pipes[i].x + pipes[i].width ||
-			this.x + this.width < pipes[i].x || 
-			this.y > pipes[i].y + pipes[i].height ||
-			this.y + this.height < pipes[i].y
-			)){
-			return true;
-	}
-}
+Bird.prototype.isDead = function(){
+	return this.weight < 50 || this.weight > 100;
 }
 
-var Pipe = function(json){
+var Food = function(json){
 	this.x = 0;
 	this.y = 0;
-	this.width = 50;
-	this.height = 40;
-	this.speed = 3;
+	this.width = 130;
+	this.height = 130;
+	this.speed = 5;
+	this.id = 1;
 
 	this.init(json);
+
+	this.isPoison = this.id % 2 === 1;
 }
 
-Pipe.prototype.init = function(json){
+Food.prototype.init = function(json){
 	for(var i in json){
 		this[i] = json[i];
 	}
 }
 
-Pipe.prototype.update = function(){
+Food.prototype.update = function(){
 	this.x -= this.speed;
 }
 
-Pipe.prototype.isOut = function(){
+Food.prototype.isOut = function(){
 	if(this.x + this.width < 0){
 		return true;
 	}
 }
 
 var Game = function(){
-	this.pipes = [];
+	this.foods = [];
 	this.birds = [];
 	this.score = 0;
 	this.canvas = document.querySelector("#flappy");
 	this.ctx = this.canvas.getContext("2d");
 	this.width = this.canvas.width;
 	this.height = this.canvas.height;
-	this.spawnInterval = 90;
-	this.interval = 0;
 	this.gen = [];
 	this.alives = 0;
 	this.generation = 0;
@@ -140,14 +133,19 @@ var Game = function(){
 }
 
 Game.prototype.start = function(){
-	this.interval = 0;
 	this.score = 0;
-	this.pipes = [];
+	this.foods = [];
 	this.birds = [];
+
+	this.nextFood = null;
 
 	this.gen = Neuvol.nextGeneration();
 	for(var i in this.gen){
-		var b = new Bird();
+		var percent = Number(i)/this.gen.length;
+		var b = new Bird({
+			x: this.width * 0.2,
+			y: this.height * percent,
+		});
 		this.birds.push(b)
 	}
 	this.generation++;
@@ -156,61 +154,52 @@ Game.prototype.start = function(){
 
 Game.prototype.update = function(){
 	this.backgroundx += this.backgroundSpeed;
-	var nextHoll = 0;
-	if(this.birds.length > 0){
-		for(var i = 0; i < this.pipes.length; i+=2){
-			if(this.pipes[i].x + this.pipes[i].width > this.birds[0].x){
-				nextHoll = this.pipes[i].height/this.height;
-				break;
-			}
-		}
-	}
 
-	for(var i in this.birds){
-		if(this.birds[i].alive){
-
-			var inputs = [
-			this.birds[i].y / this.height,
-			nextHoll
-			];
-
-			var res = this.gen[i].compute(inputs);
-			if(res > 0.5){
-				this.birds[i].flap();
-			}
-
-			this.birds[i].update();
-			if(this.birds[i].isDead(this.height, this.pipes)){
-				this.birds[i].alive = false;
-				this.alives--;
-				//console.log(this.alives);
-				Neuvol.networkScore(this.gen[i], this.score);
-				if(this.isItEnd()){
-					this.start();
-				}
-			}
-		}
-	}
-
-	for(var i = 0; i < this.pipes.length; i++){
-		this.pipes[i].update();
-		if(this.pipes[i].isOut()){
-			this.pipes.splice(i, 1);
+	for(var i = 0; i < this.foods.length; i++){
+		this.foods[i].update();
+		if(this.foods[i].isOut()){
+			this.foods.splice(i, 1);
 			i--;
 		}
 	}
 
-	if(this.interval == 0){
-		var deltaBord = 50;
-		var pipeHoll = 120;
-		var hollPosition = Math.round(Math.random() * (this.height - deltaBord * 2 - pipeHoll)) +  deltaBord;
-		this.pipes.push(new Pipe({x:this.width, y:0, height:hollPosition}));
-		this.pipes.push(new Pipe({x:this.width, y:hollPosition+pipeHoll, height:this.height}));
+	var foodEaten = false;
+
+	for(var i in this.birds){
+		if(this.birds[i].alive){
+
+			if (!this.nextFood) {
+				var id = Math.floor(Math.random() * differentTypesOfFood) + 1;
+				this.nextFood = new Food({x:this.width, y:(id-1)*this.height/differentTypesOfFood, id:id });
+				this.foods.push(this.nextFood);
+			}
+
+			if (this.nextFood.x < this.width * 0.2) {
+				foodEaten = true;
+
+				var inputs = [
+					this.nextFood.id,
+					Math.floor(this.birds[i].weight/10),
+				];
+				console.log(inputs)
+				var res = this.gen[i].compute(inputs);
+				var fed = res > 0.5;
+				this.birds[i].checkFood(this.nextFood, fed);
+			}
+
+			this.birds[i].update();
+
+			if(this.birds[i].isDead()){
+				this.birds[i].alive = false;
+				this.alives--;
+
+				Neuvol.networkScore(this.gen[i], this.score);
+			}
+		}
 	}
 
-	this.interval++;
-	if(this.interval == this.spawnInterval){
-		this.interval = 0;
+	if (foodEaten) {
+		this.nextFood = null;
 	}
 
 	this.score++;
@@ -226,8 +215,11 @@ Game.prototype.update = function(){
 			self.update();
 		}, 1000/FPS);
 	}
-}
 
+	if(this.isItEnd()){
+		this.start();
+	}
+}
 
 Game.prototype.isItEnd = function(){
 	for(var i in this.birds){
@@ -244,22 +236,40 @@ Game.prototype.display = function(){
 		this.ctx.drawImage(images.background, i * images.background.width - Math.floor(this.backgroundx%images.background.width), 0)
 	}
 
-	for(var i in this.pipes){
-		if(i%2 == 0){
-			this.ctx.drawImage(images.pipetop, this.pipes[i].x, this.pipes[i].y + this.pipes[i].height - images.pipetop.height, this.pipes[i].width, images.pipetop.height);
-		}else{
-			this.ctx.drawImage(images.pipebottom, this.pipes[i].x, this.pipes[i].y, this.pipes[i].width, images.pipetop.height);
+	for(var i in this.foods){
+		var x = this.foods[i].x;
+		var y = this.foods[i].y;
+		if (this.foods[i].isPoison) {
+			this.ctx.drawImage(images.avocadoPoison, x, y, this.foods[i].width, this.foods[i].height);
+		} else {
+			this.ctx.drawImage(images.avocado, x, y, this.foods[i].width, this.foods[i].height);
 		}
+		if (this.foods[i].isPoison) {
+			this.ctx.fillStyle = "#FF0000";
+		} else {
+			this.ctx.fillStyle = "#FFFFFF";
+		}
+		this.ctx.fillText('Alimento '+this.foods[i].id, x+130, y+50);
+		this.ctx.fillText(this.foods[i].isPoison ? 'VENENO !!!' : 'Saudavel', x+130, y+50+20);
 	}
 
-	this.ctx.fillStyle = "#FFC600";
+	this.ctx.fillStyle = "#FFFFFF";
 	this.ctx.strokeStyle = "#CE9E00";
 	for(var i in this.birds){
 		if(this.birds[i].alive){
 			this.ctx.save(); 
 			this.ctx.translate(this.birds[i].x + this.birds[i].width/2, this.birds[i].y + this.birds[i].height/2);
 			this.ctx.rotate(Math.PI/2 * this.birds[i].gravity/20);
-			this.ctx.drawImage(images.bird, -this.birds[i].width/2, -this.birds[i].height/2, this.birds[i].width, this.birds[i].height);
+
+			var maxDiff = 25;
+			var sizePercent = 1 + (this.birds[i].weight - 75)/maxDiff;
+			var width = this.birds[i].width * sizePercent;
+			var height = this.birds[i].height * sizePercent;
+			this.ctx.drawImage(images.bird, -width/2, -height/2, width, height);
+
+			this.ctx.fillText(this.birds[i].weight.toFixed(0)+'kg', 25, 8);
+			this.ctx.fillText(this.birds[i].fedLastFood ? 'nham nham' : '', 100, 8);
+
 			this.ctx.restore();
 		}
 	}
@@ -281,13 +291,13 @@ window.onload = function(){
 	var sprites = {
 		bird:"./img/bird.png",
 		background:"./img/background.png",
-		pipetop:"./img/pipetop.png",
-		pipebottom:"./img/pipebottom.png"
+		avocado:"./img/avocado.png",
+		avocadoPoison:"./img/avocado-poison.png",
 	}
 
 	var start = function(){
 		Neuvol = new Neuroevolution({
-			population:50,
+			population:10,
 			network:[2, [2], 1],
 		});
 		game = new Game();
