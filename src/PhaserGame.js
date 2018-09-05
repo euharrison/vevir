d7f3677a6fd07fbbc31dafe1d9850713533d3953
Harrison Mendonça <harrison@grama.cc>
2018-09-05 01:38:56 -0300

    fix phaser game

--- src/PhaserGame.js
+++ src/PhaserGame.js
@@ -1,80 +1,184 @@
-
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1024, 768, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
-    //  tiles are 16x16 each
-    game.load.image('tiles', 'img/sci-fi-tiles.png');
+  game.load.spritesheet('dude', 'assets/images/dude.png', 32, 48);
+  game.load.image('background', 'assets/images/background2.png');
 
+  game.load.image('player', 'img/bird.png');
+  game.load.image('wall', 'img/bird.png');
+  game.load.image('coin', 'img/bird.png');
+  game.load.image('enemy', 'img/bird.png');
 }
 
+var player;
+var facing = 'left';
+var jumpTimer = 0;
 var cursors;
+var jumpButton;
+var bg;
+
+
+var walls;
+var coins;
+var enemies;
 
 function create() {
 
-    //  Create some map data dynamically
-    //  Map size is 128x128 tiles
-    var data = '';
+  game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    for (var y = 0; y < 128; y++)
-    {
-        for (var x = 0; x < 128; x++)
-        {
-            data += game.rnd.between(0, 20).toString();
+  bg = game.add.tileSprite(0, 0, game.width, game.height, 'background');
 
-            if (x < 127)
-            {
-                data += ',';
-            }
-        }
+  player = game.add.sprite(32, 320, 'dude');
+  game.physics.enable(player, Phaser.Physics.ARCADE);
 
-        if (y < 127)
-        {
-            data += "\n";
-        }
+  player.body.collideWorldBounds = true;
+  player.body.gravity.y = 1000;
+  player.body.maxVelocity.y = 500;
+  player.body.setSize(20, 32, 5, 16);
+
+  player.animations.add('left', [0, 1, 2, 3], 10, true);
+  player.animations.add('turn', [4], 20, true);
+  player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+  cursors = game.input.keyboard.createCursorKeys();
+  jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+  walls = game.add.group();
+  coins = game.add.group();
+  enemies = game.add.group();
+
+  // Design the level. x = wall, o = coin, ! = lava.
+  var level = [
+      'xxxxxxxxxxxxxxxxxxxxxx',
+      '!         !          x',
+      '!                 o  x',
+      '!                 o  x',
+      '!                 o  x',
+      '!                 o  x',
+      '!                 o  x',
+      '!                 o  x',
+      '!                 o  x',
+      '!                 o  x',
+      '!                 o  x',
+      '!                 o  x',
+      '!                 o  x',
+      '!                 o  x',
+      '!                 o  x',
+      '!         o          x',
+      '!                    x',
+      '!     o   !    x     x',
+      'xxxxxxxxxxxxxxxx!!!!!x',
+  ];
+  const tileWidth = 20;
+  const tileHeight = 40;
+  // Create the level by going through the array
+  for (var i = 0; i < level.length; i++) {
+    for (var j = 0; j < level[i].length; j++) {
+
+      // Create a wall and add it to the 'walls' group
+      if (level[i][j] == 'x') {
+        var wall = game.add.sprite(30+tileWidth*j, 30+tileHeight*i, 'wall');
+        walls.add(wall);
+        // wall.body.immovable = true; 
+      }
+
+      // Create a coin and add it to the 'coins' group
+      else if (level[i][j] == 'o') {
+        var coin = game.add.sprite(30+tileWidth*j, 30+tileHeight*i, 'coin');
+        coins.add(coin);
+        coin
+        game.physics.arcade.enable(coin);
+      }
+
+      // Create a enemy and add it to the 'enemies' group
+      else if (level[i][j] == '!') {
+        var enemy = game.add.sprite(30+tileWidth*j, 30+tileHeight*i, 'enemy');
+        enemies.add(enemy);
+      }
     }
-
-    // console.log(data);
-
-    //  Add data to the cache
-    game.cache.addTilemap('dynamicMap', null, data, Phaser.Tilemap.CSV);
-
-    //  Create our map (the 16x16 is the tile size)
-    var map = game.add.tilemap('dynamicMap', 16, 16);
-
-    //  'tiles' = cache image key, 16x16 = tile size
-    map.addTilesetImage('tiles', 'tiles', 16, 16);
-
-    //  0 is important
-    var layer = map.createLayer(0);
-
-    //  Scroll it
-    layer.resizeWorld();
-
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    cursors = game.input.keyboard.createCursorKeys();
+  }
 
 }
 
 function update() {
 
-    if (cursors.left.isDown)
-    {
-        game.camera.x--;
-    }
-    else if (cursors.right.isDown)
-    {
-        game.camera.x++;
-    }
+  // game.physics.arcade.collide(player, layer);
+  // game.physics.arcade.collide(player, layer);
 
-    if (cursors.up.isDown)
+  // Make the player and the walls collide
+  // game.physics.arcade.collide(player, walls);
+
+  // Call the 'takeCoin' function when the player takes a coin
+  game.physics.arcade.overlap(player, coins, takeCoin, null, this)
+
+
+  // // Call the 'restart' function when the player touches the enemy
+  // game.physics.arcade.overlap(player, enemies, restart, null, this);
+
+
+  player.body.velocity.x = 0;
+
+  if (cursors.left.isDown)
+  {
+    player.body.velocity.x = -150;
+
+    if (facing != 'left')
     {
-        game.camera.y--;
+      player.animations.play('left');
+      facing = 'left';
     }
-    else if (cursors.down.isDown)
+  }
+  else if (cursors.right.isDown)
+  {
+    player.body.velocity.x = 150;
+
+    if (facing != 'right')
     {
-        game.camera.y++;
+      player.animations.play('right');
+      facing = 'right';
     }
+  }
+  else
+  {
+    if (facing != 'idle')
+    {
+      player.animations.stop();
+
+      if (facing == 'left')
+      {
+          player.frame = 0;
+      }
+      else
+      {
+          player.frame = 5;
+      }
+
+      facing = 'idle';
+    }
+  }
+  
+  if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+  {
+    player.body.velocity.y = -500;
+    jumpTimer = game.time.now + 750;
+  }
 
 }
+
+function render () {
+
+  // game.debug.text(game.time.physicsElapsed, 32, 32);
+  // game.debug.body(player);
+  game.debug.bodyInfo(player, 16, 24);
+
+}
+
+function takeCoin (player, coin) {
+
+  console.log('coin')
+
+  coin.kill();
+
+}
+
